9b44fa739216c8007ed932455e942a2ff0646d7b
Gajendra Nishad <75714258+gajjug004@users.noreply.github.com>
2023-12-08 13:36:50 +0530

    Update result_bot.py

--- result_bot.py
+++ result_bot.py
@@ -4,6 +4,7 @@ from pyrogram.types import (ReplyKeyboardMarkup, InlineKeyboardMarkup,
 import os
 import re
 import scraper as s
+import sqlite3
 
 links = s.get_latest_links()
 eid = []
@@ -18,6 +19,7 @@ app = Client(
     api_id=api_id, api_hash=api_hash,
     bot_token=bot_token
 )
+
 
 @app.on_message(filters.command("start") & filters.private)
 async def start(bot, message):
@@ -50,16 +52,25 @@ async def start(bot, message):
 
 @app.on_message(filters.regex(r"\d{6,}") & filters.private)
 async def result(client, message):
-    await client.send_message(message.chat.id, 'Processing...')
-    text = message.text
-    if re.search(r'\d{6,}', text):
-        roll = message.text
-        res = s.get_your_result(eid,roll)
-        await client.send_message(message.chat.id, res)
+    # await client.send_message(message.chat.id, 'Processing...')
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+
+    eid = get_callback_data(user_id)
+
+    if eid:
+        roll_number = message.text
+
+        res = s.get_your_result(eid,roll_number)
+        await client.send_message(chat_id, res)
+
+        clear_callback_data(user_id)
+    else :
+        await client.send_message(chat_id, 'Run the command \n/latest_result or \n/search_by_keyword \nThen select the course from given links...')
 
 @app.on_message(filters.text & filters.private)
 async def result(client, message):
-    await client.send_message(message.chat.id, 'Processing...')
+    # await client.send_message(message.chat.id, 'Processing...')
     text = message.text
     # l1 = s.for_first_page()
     l2 = s.for_all_pages()
@@ -80,18 +91,56 @@ async def result(client, message):
 
 @app.on_callback_query()
 async def answer(client, callback_query):
-    global eid
-    eid = callback_query.data
+    chat_id = callback_query.message.chat.id
+    user_id = callback_query.from_user.id
+    data = callback_query.data
+
+    store_callback_data(user_id, data)
+    # print(callback_query)
+    # global eid
+    # eid = callback_query.data
     for i in links:
-        if i['eid'] == eid:
+        if i['eid'] == data:
             eid_data = i['date'] + " " + i['course']
     await callback_query.edit_message_text(eid_data)
-    await client.send_message(callback_query.from_user.id,"Please enter your roll number:")
+    await client.send_message(chat_id,"Please enter your roll number:")
 
 
-print("I'm alive...")
+# Function to store callback data in the database
+def store_callback_data(user_id, data):
+    conn = sqlite3.connect('user_data.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO user_data (user_id, callback_data) VALUES (?, ?)", (user_id, data))
+    conn.commit()
+    conn.close()
 
-app.run()
+# Function to retrieve callback data from the database
+def get_callback_data(user_id):
+    conn = sqlite3.connect('user_data.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT callback_data FROM user_data WHERE user_id=?", (user_id,))
+    result = cursor.fetchone()
+    conn.close()
+    return result[0] if result else None
+
+# Function to clear callback data from the database
+def clear_callback_data(user_id):
+    conn = sqlite3.connect('user_data.db')
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM user_data WHERE user_id=?", (user_id,))
+    conn.commit()
+    conn.close()
+
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect('user_data.db')
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS user_data (user_id INTEGER, callback_data TEXT)")
+    conn.commit()
+    conn.close()
+    print("I'm alive...")
+    app.run()
 
 
 
