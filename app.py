9a48d4338014a5adec925d25cccf955582ee9a17
Gajendra Nishad <75714258+gajjug004@users.noreply.github.com>
2023-12-25 23:46:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import scraper as sc
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, world'
+
+@app.route('/update')
+def update_database():
+    res = sc.update_result_links()
+    return 'Update kr diya hu bhai thk h aur ye bol rha\n' + res
 
 
 if __name__ == "__main__":
